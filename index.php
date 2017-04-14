2f6a896541a8a3ea0cd3b35a7fb1a43514954426
anshul <anshul@unyscape.com>
2017-04-14 10:32:43 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+$another=$a=array(5,4,3,2,1);
+$n=5;
+for($c=0;$c<$n-1;$c++)
+{
+	for($p=0;$p<$n-$c-1;$p++)
+	{
+		if($a[$p]>$a[$p+1])
+		{
+			$t=$a[$p+1];
+			$a[$p+1]=$a[$p];
+			$a[$p]=$t;
+		}
+	}
+}
+for($c=1;$c<$n;$c++)
+{
+	$d=$c;
+	while($d > 0 && $another[$d] < $another[$d-1])
+	{
+		
+			 $t          = $another[$d];
+				$another[$d]   = $another[$d-1];
+			$another[$d-1] = $t;
+ 
+      $d--;
+		
+	}
+}
+print_r($a);
+print_r($another);
+?>
